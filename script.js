8a1356d5e89243ee5ea402c9bbe3da4f8930bad4
Gilmar Ferrari Filho <51145800+gilmarferrari@users.noreply.github.com>
2022-03-21 17:34:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ function changeLanguage() {
 
     localStorage.setItem('language', language);
 }
+
+function showAbout(number, parent) {
+    var about = document.getElementById(`about-${number}`);
+
+    about.classList.remove('hideAbout');
+    about.classList.add('about');
+    parent.classList.remove('initial-icon');
+}
