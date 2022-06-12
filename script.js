5e60d567b33f11058c4c6aa73a12a18037ef8728
Gilmar Ferrari <gilmarferrarifilho15@gmail.com>
2022-06-12 10:08:52 -0300

    Implemented link to LinkedIn.

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function changeLanguage() {
-    var language = localStorage.getItem('language');
-
-    switch (language) {
-        case 'English':
-            language = 'Portuguese'
-            break;
-        case 'Portuguese':
-            language = 'English'
-            break;
-        default:
-            language = 'Portuguese'
-            break;
-    }
-
-    localStorage.setItem('language', language);
-}
-
 function showAbout(number, parent) {
     var about = document.getElementById(`about-${number}`);
 
