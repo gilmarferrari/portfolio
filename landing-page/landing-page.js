f73d488c51ec542258b239285fb76f88759db867
Gilmar Ferrari <gilmarferrarifilho15@gmail.com>
2022-07-29 18:48:08 -0300

    Landing page improvements.

--- landing-page/landing-page.js
+++ landing-page/landing-page.js
@@ -3,3 +3,19 @@ function scroll(element) {
         behavior: 'smooth'
     });
 }
+
+var prevScrollPos = window.pageYOffset;
+
+window.onscroll = function () {
+    var navigation = document.getElementById("navigation");
+    var curScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > curScrollPos) {
+        navigation.style.top = "0";
+    }
+    else if (curScrollPos > 40) {
+        navigation.style.top = "-50px";
+    }
+    
+    prevScrollPos = curScrollPos;
+}
