a294071af592050e3cf6f20cfb29f659253e6e27
Gilmar Ferrari <gilmarferrarifilho15@gmail.com>
2022-08-27 11:32:46 -0300

    Added more typing animations.

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ function showAbout(number, parent) {
     about.classList.add('about');
     parent.classList.remove('initial-icon');
 }
+
+var index = 0;
+window.onload = changeLabel;
+
+function changeLabel() {
+    var label = document.getElementById('label');
+
+    switch (index) {
+        case 0:
+            label.innerText = 'Desenvolvedor de Software';
+            index ++;
+            break;
+        case 1:
+            label.innerText = '<html> Desenvolvedor Web </html>';
+            index ++;
+            break;
+        case 2:
+            label.innerText = '<xml> Desenvolvedor Mobile </xml>';
+            index ++;
+            break;
+        case 3:
+            label.innerText = '../Desenvolvedor de Jogos/';
+            index ++;
+            break;
+            case 4:
+            label.innerText = 'SELECT * FROM TbPortfolio';
+            index = 0;
+            break;
+    }
+    setTimeout(() => changeLabel(), 5999);
+}
