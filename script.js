b760c45261c0d4359eba3fc72785a665fa90cad6
Gilmar Ferrari <gilmarferrarifilho15@gmail.com>
2023-03-08 19:18:57 -0300

    Removed english version page. Minor information updates.

--- script.js
+++ script.js
@@ -6,46 +6,10 @@ function showAbout(number, parent) {
     parent.classList.remove('initial-icon');
 }
 
-function changeLanguage(language) {
-    switch (language) {
-        case 'PT-BR':
-            language = 'PT-BR'
-            window.location.href = portPage;
-            break;
-        case 'EN-US':
-            language = 'EN-US'
-            window.location.href = engPage;
-            break;
-    }
-
-    localStorage.setItem('lang', language);
-    getLanguage();
-}
-
-function getLanguage() {
-    var select = document.getElementById('lang-code');
-    select.value = localStorage.getItem('lang') ?? 'PT-BR';
-}
-
-function initLanguage() {
-    var language = localStorage.getItem('lang') ?? 'PT-BR';
-
-    if (language == 'PT-BR' && window.location.href != portPage) {
-        window.location.href = portPage;
-    }
-    else if (language == 'EN-US' && window.location.href != engPage) {
-        window.location.href = engPage;
-    }
-}
-
 var index = 0;
-const portPage = 'https://gilmarferrari.github.io/portfolio/';
-const engPage = 'https://gilmarferrari.github.io/portfolio/en/';
 window.onload = onInit;
 
 function onInit() {
-    initLanguage();
-    getLanguage();
     changeLabel();
 }
 
@@ -56,43 +20,21 @@ function changeLabel() {
         label.classList.add('typing-effect');
     }
 
-    var language = localStorage.getItem('lang') ?? 'PT-BR';
-
     switch (index) {
         case 0:
-            if (language == 'PT-BR') {
-                label.innerText = 'Desenvolvedor de Software';
-            }
-            else if (language == 'EN-US') {
-                label.innerText = 'Software Developer';
-            }
+            label.innerText = 'Desenvolvedor de Software';
             index++;
             break;
         case 1:
-            if (language == 'PT-BR') {
-                label.innerText = '<html> Desenvolvedor Web </html>';
-            }
-            else if (language == 'EN-US') {
-                label.innerText = '<html> Web Developer </html>';
-            }
+            label.innerText = '<html> Desenvolvedor Web </html>';
             index++;
             break;
         case 2:
-            if (language == 'PT-BR') {
-                label.innerText = '<xml> Desenvolvedor Mobile </xml>';
-            }
-            else if (language == 'EN-US') {
-                label.innerText = '<xml> Mobile Developer </xml>';
-            }
+            label.innerText = '<xml> Desenvolvedor Mobile </xml>';
             index++;
             break;
         case 3:
-            if (language == 'PT-BR') {
-                label.innerText = '../Desenvolvedor de Jogos/';
-            }
-            else if (language == 'EN-US') {
-                label.innerText = '../Game Developer/';
-            }
+            label.innerText = '../Desenvolvedor de Jogos/'
             index = 0;
             break;
     }
